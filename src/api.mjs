956daff972e9c9d2344cab1d0231bc9c40ef6344
arbrown7 <144759843+arbrown7@users.noreply.github.com>
2025-12-15 23:27:08 -0700

    when will it end

--- src/api.mjs
+++ src/api.mjs
@@ -1,12 +1,16 @@
 const API_BASE = 'https://christmas-movie-bingo-api.onrender.com/api';
 
 export async function fetchBingoPrompts() {
-  const res = await fetch(`${API_BASE}/bingo`);
+  const res = await fetch(`${API_BASE}/bingo`, {
+    credentials: 'include'
+  });
   return res.json();
 }
 
 export async function fetchMovies() {
-  const res = await fetch(`${API_BASE}/movies`);
+  const res = await fetch(`${API_BASE}/movies`, {
+    credentials: 'include'
+  });
   return res.json();
 }
 
@@ -14,6 +18,7 @@ export async function submitMovie(movieData) {
   const res = await fetch(`${API_BASE}/movies`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
+    credentials: 'include',
     body: JSON.stringify(movieData),
   });
   return res.json();
