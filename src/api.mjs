b1c815e44245f798f0db96006422156e78b928aa
arbrown7 <144759843+arbrown7@users.noreply.github.com>
2025-12-17 15:37:00 -0700

    Update api.mjs
    commented out fetch movies because I don't use that anymore

--- src/api.mjs
+++ src/api.mjs
@@ -7,12 +7,12 @@ export async function fetchBingoPrompts() {
   return res.json();
 }
 
-export async function fetchMovies() {
+/*export async function fetchMovies() {
   const res = await fetch(`${API_BASE}/movies`, {
     credentials: 'include'
   });
   return res.json();
-}
+}*/
 
 export async function submitMovie(movieData) {
   const res = await fetch(`${API_BASE}/movies`, {
